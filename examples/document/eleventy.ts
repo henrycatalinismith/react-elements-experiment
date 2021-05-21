e94f5dfd0793e26113100c8a730ddd78973ab5a8
Henry Catalini Smith <henry@catalinismith.se>
2021-05-21 22:31:08 +0200

    document example

---
+++ examples/document/eleventy.ts
@@ -0,0 +1,5 @@
+import { reactPlugin } from "@hendotcat/11tysnap"
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(reactPlugin)
+}
---
+++ examples/document/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Document } from "../../"
+
+export default function Index(): React.ReactElement {
+  return (
+    <Document>
+    </Document>
+  )
+}
